936f16bcedd364431d796c52b2dd0a9542524fd4
jagiweb <jagiweb@gmail.com>
2022-07-01 04:42:43 +0100

    some validations done for supplier modal

--- src/components/admin/supplier/CreateSupplierModal.js
+++ src/components/admin/supplier/CreateSupplierModal.js
@@ -8,9 +8,11 @@ function CreateSupplierModal(admin) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const [supplier, setSupplier] = useState()
+    const [newSupplier, setNewSupplier] = useState({success:null, message: null})
     const [AlertShow, setAlertShow] = useState(false)
     const [companies, setCompanies] = useState()
     const [company, setCompany] = useState()
+    const [validated, setValidated] = useState(false);
 
     useEffect(() => {
         let token = localStorage.token
@@ -27,19 +29,25 @@ function CreateSupplierModal(admin) {
           [name]: value,
           company_id: company
       }))
-      console.log(supplier)         
+               
     } 
 
     const hSubmit = (e) => {
         e.preventDefault()
         API.createSupplier(supplier)
-            .then(data => setSupplier({success: data.success, message: data.message}))
+            .then(data => handleAlert(data))
+        
+            
+        setValidated(true);
     }
 
-    const handleAlert = () =>{
-
-      setAlertShow(true)
-      setShow(false)
+    const handleAlert = (data) =>{
+        setNewSupplier({success: data.success, message: data.message})
+        if (data.success){
+            setAlertShow(true)
+            setShow(false)
+        }
+            
     }
 
     const selectOptions = (data) =>{
@@ -54,9 +62,10 @@ function CreateSupplierModal(admin) {
     }
   return (
     <>
-        {AlertShow && supplier.success ? <Alert onClose={() => setAlertShow(false)} dismissible variant="success">{supplier.message}</Alert> : null}
-        {AlertShow && supplier.success === false ? <Alert onClose={() => setAlertShow(false)} dismissible variant="danger">{supplier.message}</Alert> : null}
-        {console.log(companies)}
+        {console.log(newSupplier)}
+        {AlertShow && newSupplier.success ? <Alert onClose={() => setAlertShow(false)} dismissible variant="success">{newSupplier.message}</Alert> : null}
+        {AlertShow && newSupplier.success === false ? <Alert onClose={() => setAlertShow(false)} dismissible variant="danger">{newSupplier.message}</Alert> : null}
+        {console.log(company)}
         <Button className="nextButton" onClick={handleShow}>
             Crear Proveedor
         </Button>
@@ -65,37 +74,40 @@ function CreateSupplierModal(admin) {
             <Modal.Header closeButton>
                 <Modal.Title>Crear Proveedor</Modal.Title>
             </Modal.Header>
-            <Form onSubmit={hSubmit} >
+            <Form noValidate validated={validated} onSubmit={hSubmit} >
                 <Modal.Body>
                     <Container>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Nombre de proveedor *</Form.Label>
-                            <Form.Control autoComplete="off"  name="name" onChange={handleChange} type="text"  placeholder="Coloca aqui el nombre de empresa" autoFocus/>
-                        </Form.Group>
                         <Form.Group className="mb-3">
-                            <Form.Label>Direccion</Form.Label>
-                            <Form.Control autoComplete="off" name="address" onChange={handleChange} type="text" placeholder="Direccion del proveedor"/>
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Website</Form.Label>
-                            <Form.Control autoComplete="off" name="website" onChange={handleChange} type="text" placeholder="Pagina web ej: wwww.proveedor.com"/>
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>PDF</Form.Label>
-                            <Form.Control autoComplete="off" name="pdf" onChange={handleChange} type="file" placeholder=""/>
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Numero de telefono</Form.Label>
-                            <Form.Control autoComplete="off" name="phone" onChange={handleChange} type="number" placeholder="Numero telefonico 555-555-5555"/>
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>A que empresa tuya pertenece el proveedor?</Form.Label>
+                            <Form.Label>A que empresa tuya pertenece el proveedor? *</Form.Label>
                             {/* <Form.Control autoComplete="off" name="company_id" onChange={handleChange} type="text" placeholder=""/> */}
                             {companies ? <Select 
+                                            required
                                             name={"company_id"} 
                                             onChange={value => setCompany(value.value)} 
                                             options={selectOptions(companies)} /> : null}
                         </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Nombre de proveedor *</Form.Label>
+                            <Form.Control required autoComplete="off"  name="name" onChange={handleChange} type="text"  placeholder="Coloca aqui el nombre de empresa" autoFocus/>
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Direccion</Form.Label>
+                            <Form.Control  autoComplete="off" name="address" onChange={handleChange} type="text" placeholder="Direccion del proveedor"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Website</Form.Label>
+                            <Form.Control  autoComplete="off" name="website" onChange={handleChange} type="text" placeholder="Pagina web ej: wwww.proveedor.com"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>PDF</Form.Label>
+                            <Form.Control  autoComplete="off" name="pdf" onChange={handleChange} type="file" placeholder=""/>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Numero de telefono</Form.Label>
+                            <Form.Control required autoComplete="off" name="phone" onChange={handleChange} type="number" placeholder="Numero telefonico 555-555-5555"/>
+                        </Form.Group>
+                        
                     </Container>
                 </Modal.Body>
                 <Modal.Footer>
